58b0f10203a8ead52b39f99dbc57e3f6ef9ab24a
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2024-01-27 19:29:32 +0530

    create file upload project

--- app.ts
+++ app.ts
@@ -1,10 +1,45 @@
 import express from "express";
+import multer from "multer";
+import fs from "fs";
+import { dirname, join, resolve } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const __dirname = resolve(dirname(fileURLToPath(import.meta.url)), "..");
 
-app.use("/", (req, res) => {
-  res.json({ message: "file upload demo using multer" });
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, "uploads")));
+app.use(express.static(join(__dirname, "public")));
+
+console.log(join(__dirname, "public"));
+
+const storage = multer.diskStorage({
+  destination: (req, res, callback) => {
+    callback(null, "./uploads");
+  },
+  filename: (req, res, callback) => {
+    callback(null, `${Date.now()}-${res.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.get("/upload", (req, res) => {
+  fs.readdir("./uploads/", (err, data) => {
+    console.log(data);
+    res.json({ imageUrl: join(__dirname, "uploads", data[0]) });
+  });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req.file, req.body);
+  res.send("demo...");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
